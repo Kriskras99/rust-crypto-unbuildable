8c038be2017eaa28cd3a608dcf33519d9ac3811e
Christiaan Biesterbosch <github@kriskras99.nl>
2022-03-07 19:41:14 +0100

    Heapless?

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #![no_std]
 
 use core::panic::PanicInfo;
-use chacha20poly1305;
+use heapless::Vec; // fixed capacity `std::Vec`
 
 #[panic_handler]
 pub fn panic(_panic: &PanicInfo<'_>) -> ! {
@@ -10,5 +10,10 @@ pub fn panic(_panic: &PanicInfo<'_>) -> ! {
 }
 
 pub extern "C" fn main() -> ! {
+    // on the stack
+    let mut xs: Vec<u8, 8> = Vec::new(); // can hold up to 8 elements
+    xs.push(42).unwrap();
+    assert_eq!(xs.pop(), Some(42));
+
     loop {}
 }
